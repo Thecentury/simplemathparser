f8eb1709b9deed7565087a285d30322bd2d79169
Thecentury_cp <SND\Thecentury_cp>
2010-09-13 17:30:16 +0000

    Fixed error while parsing negative numbers, added unary negation support.

---
+++ Parser/Parser/SyntaxTokenReaders/ConstantSyntaxTokenReader.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MathParser.SyntaxTokens;
+using AST = MathParser.Tree<MathParser.SyntaxToken>;
+
+namespace MathParser.SyntaxTokenReaders
+{
+	public class ConstantSyntaxTokenReader : SyntaxTokenReader
+	{
+		public override AST Read(LinkedList<MixedToken> tokens, Grammar grammar)
+		{
+			var node = tokens.FindFirst(t => t.IsLexicToken && t.LexicToken is DoubleToken);
+			if (node != null)
+			{
+				var doubleToken = node.Value.LexicToken as DoubleToken;
+				AST tree = new AST(new DoubleConstantSyntaxToken { Value = doubleToken.Value });
+
+				node.Value.Tree = tree;
+
+				return tree;
+			}
+			return null;
+		}
+	}
+}
---
+++ Parser/Parser/SyntaxTokenReaders/SyntaxTokenReader.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AST = MathParser.Tree<MathParser.SyntaxToken>;
+
+namespace MathParser
+{
+	public abstract class SyntaxTokenReader
+	{
+		public double Priority { get; set; }
+
+		public abstract AST Read(LinkedList<MixedToken> tokens, Grammar grammar);
+	}
+}
